c2a69ce89337e33e6bf98268ddaba17f12a8a78d
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-18 15:51:36 +0200

    update api project controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,15 +10,21 @@ class ProjectController extends Controller
     
     public function index()
     {
-      $projects = Project::with('type', 'technologies')->paginate(4);
+      $projects = Project::with('type', 'technologies')->paginate(3);
     
-    return response()->json($projects);
+    return response()->json([
+      'success'   => true,
+      'results'   => $projects,
+    ]);
     }
 
     public function show($slug)
     {
       $project = Project::where('slug', $slug)->firstOrFail();
-      return response()->json($project);
+      return response()->json([
+        'success'   => $project ? true : false,
+        'results'   => $project,
+      ]);
   }
     }
 
